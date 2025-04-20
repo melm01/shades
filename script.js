092b27e092086a0fb4d4d51bbffa9b4cd0ae0002
melm01 <108314174+melm01@users.noreply.github.com>
2025-04-20 15:59:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function showAlert() {
   alert("Button clicked!");
+  <script>
+  window.addEventListener('load', function () {
+    const popup = document.getElementById('popup-banner');
+    const closeBtn = document.getElementById('close-popup');
+
+    popup.style.display = 'block';
+
+    closeBtn.addEventListener('click', function () {
+      popup.style.display = 'none';
+    });
+  });
+</script>
+
 }
